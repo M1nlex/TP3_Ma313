6d82e60c459811ad92f6a23eb5d3e514b01747e9
rabonarca <romaric.dubois17062000@gmail.com>
2020-11-12 11:21:37 +0100

    ex1

--- function.py
+++ function.py
@@ -1,4 +1,21 @@
 import numpy as np
 
+def migenerale(m, n, b, x0, epsilon, nitermax):
+    i = 0
+    e = 10
+    while i < nitermax or e <= epsilon:
+        i += 1
+        u = n * x0 + b
+
+        x1 = np.linalg.solve(m, u)
+
+        e = abs(x1-x0)
+        x0 = x1
+
+        print(i)
+
+    return x1, i, e
+
+
 def test1():
     print("oui")
